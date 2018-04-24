2c432fe8187c0093cd7883acce366dcefb2ebaf8
Kashif Minhaj <dailypctips@gmail.com>
2018-04-24 12:41:48 +0530

    example: Display recognitions in a ListView

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,14 +14,15 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List<Recognition> _recognitions;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    testStuff();
+    loadRecognitions();
   }
 
-  Future<Null> testStuff() async {
+  Future<Null> loadRecognitions() async {
     var classifier = await TFLiteImageClassifier.createInstance(
       assets: rootBundle,
       modelPath: "assets/mobilenet_quant_v1_224.tflite",
@@ -32,7 +33,9 @@ class _MyAppState extends State<MyApp> {
     var imageBytes = (await rootBundle.load("assets/cat500.png")).buffer;
     img.Image image = img.decodePng(imageBytes.asUint8List());
     image = img.copyResize(image, 224, 224);
-    await classifier.recognizeImage(image);
+    _recognitions = await classifier.recognizeImage(image);
+    setState(() {});
+
     await classifier.close();
   }
 
@@ -42,8 +45,20 @@ class _MyAppState extends State<MyApp> {
       title: "TFLite",
       theme: new ThemeData.light(),
       home: new Scaffold(
-        appBar: new AppBar(title: new Text("TFLite Flutter"),),
-        body: new Center(child: new Text("Hello "),),
+          appBar: new AppBar(title: new Text("TFLite Flutter"),),
+          body: _recognitions == null ? new Center(
+            child: new CircularProgressIndicator(),)
+              : new ListView.builder(
+            itemCount: _recognitions.length,
+            itemBuilder: (BuildContext ctx, int index) {
+              var item = _recognitions[index];
+              return new ListTile(
+                leading: new Text(item.id),
+                title: new Text(item.title),
+                trailing: new Text(item.confidence.toString()),
+              );
+            },
+          )
       ),
     );
   }
